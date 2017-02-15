96e0ab8a6201e438cf91c27d678c38ad1c11f6e6
gmyth <332087251@qq.com>
2017-02-15 14:15:23 -0500

    stoplight'

--- kern/synchprobs/stoplight.c
+++ kern/synchprobs/stoplight.c
@@ -75,11 +75,40 @@
 
 
 
-
+// static struct lock *intersection_lock;
+static struct lock *car0_lock;
+static struct lock *car1_lock;
+static struct lock *car2_lock;
+static struct lock *car3_lock;
+static struct cv *car0_cv;
+static struct cv *car1_cv;
+static struct cv *car2_cv;
+static struct cv *car3_cv;
+bool intersection[4];
+static struct cv *cars_cv[4];
+bool car_path[4][4];
 
 void
 stoplight_init() {
+	 intersection_lock = lock_create("intersection_lock");
+	 car0_lock = lock_create("car0_lock");
+	 car1_lock = lock_create("car1_lock");
+	 car2_lock = lock_create("car2_lock");
+	 car3_lock = lock_create("car3_lock");
+	 cars_cv[0] = cv_create("car0_cv");
+	 cars_cv[1] = cv_create("car1_cv");
+	 cars_cv[2] = cv_create("car2_cv");
+	 cars_cv[3] = cv_create("car3_cv");
+   intersection[0] = false;
+	 intersection[1] = false;
+	 intersection[2] = false;
+	intersection[3] = false;
 
+	 for(int i = 0; i < 4; ++i){
+		for(int j = 0; j < 4; ++j){
+	 		car_path[i][j] = false;
+	}
+ }
 
 	return;
 }
@@ -90,15 +119,64 @@ stoplight_init() {
 
 void stoplight_cleanup() {
 
+	intersection_lock = lock_create("intersection_lock");
+		car0_lock = lock_create("car0_lock");
+		car1_lock = lock_create("car1_lock");
+		car2_lock = lock_create("car2_lock");
+		car3_lock = lock_create("car3_lock");
 
+		cars_cv[0] = cv_create("car0_cv");
+		cars_cv[1] = cv_create("car1_cv");
+		cars_cv[2] = cv_create("car2_cv");
+		cars_cv[3] = cv_create("car3_cv");
+
+		intersection[0] = false;
+		intersection[1] = false;
+		intersection[2] = false;
+		intersection[3] = false;
+
+		for(int i = 0; i < 4; ++i){
+			for(int j = 0; j < 4; ++j){
+	 		car_path[i][j] = false;
+		}
+	}
 	return;
 }
 
 void
 turnright(uint32_t direction, uint32_t index)
 {
-	(void) direction;
-	(void) index;
+	car_path[index][direction] = true;
+
+	lock_acquire(intersection_lock);
+
+	if(intersection[direction]){
+		cv_wait(cars_cv[index],intersection_lock);
+	}
+
+	intersection[direction] = true; // intersection occupied
+
+
+	inQuadrant(direction, index);
+	leaveIntersection(index);
+	intersection[index] = false;
+
+	car_path[index][direction] = false;
+	/*helper function*/
+	for(int c = 0; c < 4; ++c){
+		bool flag = false;
+		for(int i = 0; i < 4; ++i){
+			// check direction
+			if(car_path[c][i] && (!intersection[i])){
+				flag = true;
+			}
+			if(flag){
+				cv_signal(cars_cv[c], intersection_lock);
+				break;
+			}
+		}
+	}
+	lock_release(intersection_lock);
 
 
 	return;
@@ -106,8 +184,39 @@ turnright(uint32_t direction, uint32_t index)
 void
 gostraight(uint32_t direction, uint32_t index)
 {
-	(void) direction;
-	(void)index;
+	car_path[index][direction] = true;
+
+	lock_acquire(intersection_lock);
+
+	if(intersection[direction]){
+		cv_wait(cars_cv[index],intersection_lock);
+	}
+
+	intersection[direction] = true; // intersection occupied
+
+
+	inQuadrant(direction, index);
+	leaveIntersection(index);
+	intersection[index] = false;
+
+	car_path[index][direction] = false;
+	/*helper function*/
+	for(int c = 0; c < 4; ++c){
+		bool flag = false;
+		for(int i = 0; i < 4; ++i){
+			// check direction
+			if(car_path[c][i] && (!intersection[i])){
+				flag = true;
+			}
+			if(flag){
+				cv_signal(cars_cv[c], intersection_lock);
+				break;
+			}
+		}
+	}
+	/*helper function*/
+	lock_release(intersection_lock);
+
 
 
 	return;
