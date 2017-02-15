27560fd4dc6a2189eae4e13e4b7bbca650f8fd76
gmyth <332087251@qq.com>
2017-02-15 17:03:06 -0500

    test

--- kern/thread/synch.c
+++ kern/thread/synch.c
@@ -405,14 +405,18 @@ rwlock_create(const char *name)
 void rwlock_destroy(struct rwlock *rwlock)
 {
 	KASSERT(rwlock != NULL);
-	KASSERT(rwlock->rw_reader_in_held < 0);
-	KASSERT(rwlock->rw_writer_in_queue < 0);
-	KASSERT(rwlock->rw_reader_in_queue < 0);
-	KASSERT(rwlock->rw_writer_in_held < 0);
+	// KASSERT(rwlock->rw_reader_in_held < 0);
+	// KASSERT(rwlock->rw_writer_in_queue < 0);
+	// KASSERT(rwlock->rw_reader_in_queue < 0);
+	// KASSERT(rwlock->rw_writer_in_held < 0);
 	//check the rwlock is existing
 	//release all requesting resource
 	cv_destroy(	rwlock->rw_to_write);
 	cv_destroy(	rwlock->rw_to_read);
+	kfree(rwlock->rw_reader_in_held);
+	kfree(rwlock->rw_writer_in_queue);
+	kfree(rwlock->rw_reader_in_queue);
+	kfree(rwlock->rw_writer_in_held);
 	spinlock_cleanup(&rwlock->rw_spinlock);
 	lock_destroy(rwlock->rw_lock);
 	kfree(rwlock->rwlock_name);
