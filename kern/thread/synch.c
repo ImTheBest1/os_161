857c2d667819161e1c0a9ea756ff18a2e1479888
gmyth <332087251@qq.com>
2017-02-15 17:30:06 -0500

    test

--- kern/thread/synch.c
+++ kern/thread/synch.c
@@ -422,9 +422,9 @@ void rwlock_destroy(struct rwlock *rwlock)
 void rwlock_acquire_read(struct rwlock *rwlock)
 {
 	KASSERT(rwlock != NULL);
-	// KASSERT(rwlock->rw_reader_in_held < 0);
+	KASSERT(rwlock->rw_reader_in_held < 0);
 	// KASSERT(rwlock->rw_writer_in_queue < 0);
-	// KASSERT(rwlock->rw_reader_in_queue < 0);
+	KASSERT(rwlock->rw_reader_in_queue < 0);
 	// KASSERT(rwlock->rw_writer_in_held < 0);
 	lock_acquire(rwlock->rw_lock);
 	rwlock->rw_reader_in_queue++;//add the pending queue first
@@ -440,9 +440,9 @@ void rwlock_acquire_read(struct rwlock *rwlock)
 void rwlock_release_read(struct rwlock *rwlock)
 {
 	KASSERT(rwlock != NULL);
-	//KASSERT(rwlock->rw_reader_in_held < 0);
+	KASSERT(rwlock->rw_reader_in_held < 0);
 	// KASSERT(rwlock->rw_writer_in_queue < 0);
-	// KASSERT(rwlock->rw_reader_in_queue < 0);
+	   KASSERT(rwlock->rw_reader_in_queue < 0);
 	// KASSERT(rwlock->rw_writer_in_held < 0);
 	//add
 	lock_acquire(rwlock->rw_lock);
@@ -463,8 +463,8 @@ void rwlock_acquire_write(struct rwlock *rwlock)
 	KASSERT(rwlock != NULL);
 	//KASSERT(rwlock->rw_reader_in_held < 0);
 	KASSERT(rwlock->rw_writer_in_queue < 0);
-	KASSERT(rwlock->rw_reader_in_queue < 0);
-	// KASSERT(rwlock->rw_writer_in_held < 0);
+	//KASSERT(rwlock->rw_reader_in_queue < 0);
+	KASSERT(rwlock->rw_writer_in_held < 0);
 	lock_acquire(rwlock->rw_lock);
 	rwlock->rw_writer_in_queue++; //add to pending queue
 	while(rwlock->rw_writer_in_held > 0 || rwlock->rw_reader_in_held > 0 || rwlock->rw_reader_in_queue > 0){
@@ -480,8 +480,8 @@ void rwlock_release_write(struct rwlock *rwlock)
 	KASSERT(rwlock != NULL);
 	//KASSERT(rwlock->rw_reader_in_held < 0);
 	KASSERT(rwlock->rw_writer_in_queue < 0);
-	KASSERT(rwlock->rw_reader_in_queue < 0);
-	// KASSERT(rwlock->rw_writer_in_held < 0);
+	//KASSERT(rwlock->rw_reader_in_queue < 0);
+	KASSERT(rwlock->rw_writer_in_held < 0);
 	lock_acquire(rwlock->rw_lock);
 	rwlock->rw_writer_in_held--;
 
