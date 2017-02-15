d5d187878784ef78183fe87530dae8d41b5bb62a
gmyth <332087251@qq.com>
2017-02-15 18:21:53 -0500

    test

--- kern/thread/synch.c
+++ kern/thread/synch.c
@@ -449,7 +449,7 @@ void rwlock_release_read(struct rwlock *rwlock)
 	//KASSERT(rwlock->rw_reader_in_held <= 0);
 	// KASSERT(rwlock->rw_writer_in_queue < 0);
 	//KASSERT(rwlock->rw_reader_in_queue <= 0);
-	KASSERT(rwlock->rw_writer_in_held < 0);
+	//KASSERT(rwlock->rw_writer_in_held < 0);
 	//add
 	lock_acquire(rwlock->rw_lock);
 	rwlock->rw_reader_in_held--;
@@ -490,7 +490,6 @@ void rwlock_release_write(struct rwlock *rwlock)
 	KASSERT(rwlock->rw_writer_in_held <= 0);
 	lock_acquire(rwlock->rw_lock);
 	rwlock->rw_writer_in_held--;
-
 	  // if still some writer in queue
 	if(rwlock->rw_reader_in_queue > 0){
 		cv_broadcast(rwlock->rw_to_read,rwlock->rw_lock);
